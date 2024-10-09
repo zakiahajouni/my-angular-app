09bf616983262d65d5e136d3f140b34f1b71c879
zakiahajouni <zakiahajouni362@gmail.com>
2024-10-09 22:39:27 +0100

    login component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+  { path: 'sign-up', loadChildren: () => import('./pages/sign-up/sign-up.module').then(m => m.SignUpModule) },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
